0819c5019da92599eacbd8457421ff2703f6d1da
cygnus-dev <thunder.lop@gmail.com>
2020-07-15 22:12:37 +0200

    Add settings file and environment variables

--- bot.py
+++ bot.py
@@ -1,4 +1,32 @@
 import discord
+import os
+import settings
+
+from discord.ext import commands
 
 
-print("Init Discord")
+bot = commands.Bot(command_prefix='>')
+
+
+@bot.event
+async def on_ready():
+    print("bot is working!")
+
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined the server')
+
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} has left the server')
+
+
+# TODO remove this after testing
+@bot.command
+async def ping(ctx):
+    print("hello")
+
+
+bot.run(os.getenv("TOKEN"))
