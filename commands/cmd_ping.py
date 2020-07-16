69e760832eaf22a3220d4f0cefc47accfbc316a1
cygnus-dev <thunder.lop@gmail.com>
2020-07-16 17:27:43 +0200

    Add a lambda function

--- commands/cmd_ping.py
+++ commands/cmd_ping.py
@@ -1,2 +1,8 @@
+from typing import Any, Callable
+
+
 async def run(ctx, latency):
-    await ctx.send(f'pong! {round(latency * 1000)}ms')
+    await ctx.send(f'pong! ```{calculated_latency(latency)} ms```')
+
+
+calculated_latency: Callable[[Any], int] = lambda latency: round(latency * 1000)
