dd5dd7d045a48fda36dd9b69c4950992fd460adb
cygnus-dev <thunder.lop@gmail.com>
2020-07-17 22:27:04 +0200

    faultly 8ball command

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
+import random
 
 from discord.ext import commands
-from commands import cmd_elpotato, cmd_ping
+from commands import cmd_elpotato, cmd_ping, cmd_8ball
 from events import member_join, member_remove, ready
 
 
@@ -31,3 +32,7 @@ async def patato(ctx):
 @potato.command()
 async def ping(ctx):
     await cmd_ping.run(ctx, potato.latency)
+
+@potato.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    await cmd_8ball.run(ctx, question)
