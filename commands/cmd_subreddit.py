229e0975072216b75ff22aee8cd46191e7300a4c
cygnus-dev <thunder.lop@gmail.com>
2020-07-18 17:46:57 +0200

    add reddit command

---
+++ commands/cmd_subreddit.py
@@ -0,0 +1,9 @@
+import praw
+import os
+
+async def run(ctx, topic):
+    reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT_ID"),
+                         client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+                         user_agent="USERAGENT")
+    for submission in reddit.subreddit(topic).hot(limit=2):
+        await ctx.send(submission.url)
