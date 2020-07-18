b1de317ed9b231915c1bccb04008726e32558242
cygnus-dev <thunder.lop@gmail.com>
2020-07-18 19:49:38 +0200

    subreddit command imporved

--- commands/cmd_subreddit.py
+++ commands/cmd_subreddit.py
@@ -1,10 +1,21 @@
 import praw
 import os
+import random
+
+from random import randint
 
 
 async def run(ctx, topic):
     reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT_ID"),
                          client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
                          user_agent="USERAGENT")
-    for submission in reddit.subreddit(topic).hot(limit=1):
-        await ctx.send(submission.url)
+    random_index = randint(1, 100)
+    submissions = reddit.subreddit(topic).hot(limit=random_index)
+    await ctx.send(last_submission(submissions).url)
+
+
+def last_submission(submissions):
+    submission = next(submissions)
+    for submission in submissions:
+        pass
+    return submission
