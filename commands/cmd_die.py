98adfa45bd33b059fa62844bac9f486e57fdd6f0
cygnus-dev <thunder.lop@gmail.com>
2020-07-23 20:22:54 +0200

    die modified

--- commands/cmd_die.py
+++ commands/cmd_die.py
@@ -1,4 +1,20 @@
+import random
+
+shit_posts = ['https://www.youtube.com/watch?v=D-UmfqFjpl0',
+              'https://www.youtube.com/watch?v=C9oVVBKiWnY',
+              'https://www.youtube.com/watch?v=FveF-we6lcE',
+              'https://www.youtube.com/watch?v=sIlNIVXpIns',
+              'https://www.youtube.com/watch?v=dWNvlyycWzQ',
+              'https://www.youtube.com/watch?v=Jo-0iARNt6M',
+              'https://www.youtube.com/watch?v=6G4QhXWa22c',
+              'https://www.youtube.com/watch?v=E4u32rmY_CA',
+              'https://www.youtube.com/watch?v=K9AY5rlosRY',
+              'https://www.youtube.com/watch?v=Kg-HHXuOBlw']
+
+
 
 async def run(ctx):
-    await ctx.send("```Here you go:```" + "https://www.youtube.com/watch?v=D-UmfqFjpl0")
+    await ctx.send("```Here you go:```" + (random_shitpost()))
 
+def random_shitpost():
+    return f'{random.choice(shit_posts)}'
