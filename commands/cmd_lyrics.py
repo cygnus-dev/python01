c2c8773848a3236a678c846864ca275472ccea39
cygnus-dev <thunder.lop@gmail.com>
2020-11-01 18:26:55 +0100

    fix cmd lyrics

--- commands/cmd_lyrics.py
+++ commands/cmd_lyrics.py
@@ -8,7 +8,7 @@ URL = 'https://some-random-api.ml/lyrics?title='
 async def run(ctx, song):
     r = requests.get(url=URL+song)
     data = r.json()
-    res = len(data['lyrics'].split())
+    res = len(data['lyrics'])
 
     # if int(res) < 1000:
     #     embed1 = discord.Embed(
@@ -21,48 +21,148 @@ async def run(ctx, song):
     #     await ctx.send(embed=embed1)
     # else:
     x = wrap(data['lyrics'], 1000)
-    embed2 = discord.Embed(
-        color=discord.Colour.dark_orange(),
-        # title=data['title'],
-        description=f"by {data['author']}"
-    )
 
-    embed2.add_field(name=data['title'], value=x[0])
+    if int(res) < 1000:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=f"by {data['author']}"
+        )
 
-    embed3 = discord.Embed(
-        color=discord.Colour.dark_orange(),
-        # title=data['title'],
-        description=f"by {data['author']}"
-    )
+        embed2.add_field(name=data['title'], value=x[0])
+        await ctx.send(embed=embed2)
+    elif 1000 < int(res) < 2000:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
 
-    embed3.add_field(name=data['title'], value=x[1])
+        embed2.add_field(name=data['title'], value=x[0])
+        # await ctx.send(embed=embed2)
 
-    embed4 = discord.Embed(
-        color=discord.Colour.dark_orange(),
-        # title=data['title'],
-        description=f"by {data['author']}"
-    )
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[1]
+        )
 
-    embed4.add_field(name=data['title'], value=x[2])
+        # embed3.add_field(name=data['title'], value=x[1])
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+    elif 2000 < int(res) < 3000:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
 
-    # embed5 = discord.Embed(
-    #     color=discord.Colour.dark_orange(),
-    #     # title=data['title'],
-    #     description=f"by {data['author']}"
-    # )
-    #
-    # embed5.add_field(name=data['title'], value=x[3])
-    #
-    # embed6 = discord.Embed(
-    #     color=discord.Colour.dark_orange(),
-    #     # title=data['title'],
-    #     description=f"by {data['author']}"
-    # )
-    #
-    # embed6.add_field(name=data['title'], value=x[4])
+        embed2.add_field(name=data['title'], value=x[0])
+        # await ctx.send(embed=embed2)
 
-    await ctx.send(embed=embed2)
-    await ctx.send(embed=embed3)
-    await ctx.send(embed=embed4)
-    await ctx.send(embed=embed5)
-    await ctx.send(embed=embed6)
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[1]
+        )
+
+        # embed3.add_field(name=data['title'], value=x[1])
+        # await ctx.send(embed=embed3)
+        embed4 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[2]
+        )
+
+        # embed4.add_field(name=data['title'], value=x[2])
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+        await ctx.send(embed=embed4)
+    elif 3000 < int(res) < 4000:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
+
+        embed2.add_field(name=data['title'], value=x[0])
+        # await ctx.send(embed=embed2)
+
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[1]
+        )
+
+        # embed3.add_field(name=data['title'], value=x[1])
+        # await ctx.send(embed=embed3)
+        embed4 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[2]
+        )
+
+        # embed4.add_field(name=data['title'], value=x[2])
+        # await ctx.send(embed=embed4)
+        embed5 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[3]
+        )
+
+        # embed5.add_field(name=data['title'], value=x[3])
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+        await ctx.send(embed=embed4)
+        await ctx.send(embed=embed5)
+    else:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
+
+        embed2.add_field(name=data['title'], value=x[0])
+        # await ctx.send(embed=embed2)
+
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[1]
+        )
+
+        # embed3.add_field(name=data['title'], value=x[1])
+        # await ctx.send(embed=embed3)
+        embed4 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[2]
+        )
+
+        # embed4.add_field(name=data['title'], value=x[2])
+        # await ctx.send(embed=embed4)
+        embed5 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[3]
+        )
+
+        # embed5.add_field(name=data['title'], value=x[3])
+        # await ctx.send(embed=embed5)
+        embed6 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            # title=data['title'],
+            description=x[4]
+        )
+
+        # embed6.add_field(name=data['title'], value=x[4])
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+        await ctx.send(embed=embed4)
+        await ctx.send(embed=embed5)
+        await ctx.send(embed=embed6)
+    # await ctx.send(embed=embed2)
+    # await ctx.send(embed=embed3)
+    # await ctx.send(embed=embed4)
+    # await ctx.send(embed=embed5)
+    # await ctx.send(embed=embed6)
