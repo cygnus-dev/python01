d8b03448f1fd0ddca81b17eeb9a394c662b413d5
cygnus-dev <thunder.lop@gmail.com>
2020-11-02 22:03:19 +0100

    clean cmd lyrics

--- commands/cmd_clyrics.py
+++ commands/cmd_clyrics.py
@@ -87,7 +87,8 @@ async def run(ctx, music):
         await ctx.send(embed=embed3)
         await ctx.send(embed=embed4)
         await ctx.send(embed=embed5)
-    else:
+
+    elif 4000 < int(res) < 5000:
         embed2 = discord.Embed(
             color=discord.Colour.dark_orange(),
             description=f"by {data['author']}, seperated into different messages cause length"
@@ -120,3 +121,89 @@ async def run(ctx, music):
         await ctx.send(embed=embed4)
         await ctx.send(embed=embed5)
         await ctx.send(embed=embed6)
+
+    elif 5000 < int(res) < 6000:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
+
+        embed2.add_field(name=data['title'], value=x[0])
+
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[1]
+        )
+
+        embed4 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[2]
+        )
+
+        embed5 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[3]
+        )
+
+        embed6 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[4]
+        )
+
+        embed7 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[5]
+        )
+
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+        await ctx.send(embed=embed4)
+        await ctx.send(embed=embed5)
+        await ctx.send(embed=embed6)
+        await ctx.send(embed=embed7)
+
+    else:
+        embed2 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=f"by {data['author']}, seperated into different messages cause length"
+        )
+
+        embed2.add_field(name=data['title'], value=x[0])
+
+        embed3 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[1]
+        )
+
+        embed4 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[2]
+        )
+
+        embed5 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[3]
+        )
+
+        embed6 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[4]
+        )
+
+        embed7 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[5]
+        )
+
+        embed8 = discord.Embed(
+            color=discord.Colour.dark_orange(),
+            description=x[6]
+        )
+
+        await ctx.send(embed=embed2)
+        await ctx.send(embed=embed3)
+        await ctx.send(embed=embed4)
+        await ctx.send(embed=embed5)
+        await ctx.send(embed=embed6)
+        await ctx.send(embed=embed7)
+        await ctx.send(embed=embed8)
