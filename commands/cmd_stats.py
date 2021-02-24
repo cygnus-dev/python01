e0111ef7031f2c931d433d3dc6449c6740a7880e
cygnus-dev <68337678+cygnus-dev@users.noreply.github.com>
2021-02-24 12:55:42 +0100

    imporved cmd stats

--- commands/cmd_stats.py
+++ commands/cmd_stats.py
@@ -1,6 +1,8 @@
 import psutil
 import os
 import platform
+import GPUtil
+#pip3 install tabulate, gputil
 
 async def run(ctx):
     cpufreq = psutil.cpu_freq()
@@ -9,16 +11,8 @@ async def run(ctx):
     await ctx.send(f"Total cores: {psutil.cpu_count(logical=False)}")
     await ctx.send(f"Current Frequency: {cpufreq.current:.2f}Mhz")
     await ctx.send(f"Total meroy: {get_size(svmem.total)}")
-    await ctx.send(f"available memroy: {get_size(svmem.available)}")
-    # await ctx.send(f"System: {uname.system}")
-    # await ctx.send(f"Node Name: {uname.node}")
-    # await ctx.send(f"Release: {uname.release}")
-    # await ctx.send(f"Version: {uname.version}")
-    # await ctx.send(f"Machine: {uname.machine}")
-    # await ctx.send(f"Processor: {uname.processor}")
-    # await ctx.send(cpu_usage)
-    # await ctx.send(core_count)
-    # await ctx.send(stats)
+    await ctx.send(f"available memroy: {get_size(svmem.available)}/ Used: ({svmem.percent}%)")
+    
 
 def get_size(bytes, suffix="B"):
     #copied code, and copied with absolotuly no shame
