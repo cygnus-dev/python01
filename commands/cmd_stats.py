5026d9497726d8902370ee0a1a0703474c1a3d11
cygnus-dev <68337678+cygnus-dev@users.noreply.github.com>
2021-02-24 11:46:18 +0100

    improve cmd stats

--- commands/cmd_stats.py
+++ commands/cmd_stats.py
@@ -1,7 +1,29 @@
 import psutil
+import os
+import platform
 
 async def run(ctx):
-    cpu_usage = psutil.cpu_percent
-    core_count = psutil.cpu_count
-    await ctx.send(cpu_usage)
-    await ctx.send(core_count)
+    cpufreq = psutil.cpu_freq()
+    uname = platform.uname()
+    svmem = psutil.virtual_memory()
+    await ctx.send(f"Total cores: {psutil.cpu_count(logical=False)}")
+    await ctx.send(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+    await ctx.send(f"Total meroy: {get_size(svmem.total)}")
+    await ctx.send(f"available memroy: {get_size(svmem.available)}")
+    # await ctx.send(f"System: {uname.system}")
+    # await ctx.send(f"Node Name: {uname.node}")
+    # await ctx.send(f"Release: {uname.release}")
+    # await ctx.send(f"Version: {uname.version}")
+    # await ctx.send(f"Machine: {uname.machine}")
+    # await ctx.send(f"Processor: {uname.processor}")
+    # await ctx.send(cpu_usage)
+    # await ctx.send(core_count)
+    # await ctx.send(stats)
+
+def get_size(bytes, suffix="B"):
+    #copied code, and copied with absolotuly no shame
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
